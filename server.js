a1ec680741ccc4b67e3571aa7c47f242258887a7
azadpunchy <muhammadmananbari@gmail.com>
2023-03-05 11:56:05 +0500

    listening after mongodb connection for serverless

--- server.js
+++ server.js
@@ -36,17 +36,17 @@ mongoose.connect(
   (err) => {
     if (err) throw err;
     console.log("Connected to MongoDB");
+
+    if (process.env.NODE_ENV === "production") {
+      app.use(express.static("client/build"));
+      app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+      });
+    }
+
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => {
+      console.log("Server is running on port", PORT);
+    });
   }
 );
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-  });
-}
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log("Server is running on port", PORT);
-});
