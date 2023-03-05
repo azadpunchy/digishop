0b22a9e0e46c8f77cfca4b098a0b3d622ab5710d
Manan Bari <40257979+azadpunchy@users.noreply.github.com>
2023-03-05 12:12:38 +0500

    Update server.js

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ mongoose.connect(
       });
     }
 
-    const PORT = process.env.PORT || 5000;
+    const PORT = 5000;
     app.listen(PORT, () => {
       console.log("Server is running on port", PORT);
     });
